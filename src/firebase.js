85bc30d4f005c604352955d0a70fa65798ec5254
Jill Williams <codeGeek.03.14@gmail.com>
2020-11-26 17:48:20 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDxGPt-M65zTgnCMA77UBRmV6WrXv5dwXw",
-    authDomain: "store-499f8.firebaseapp.com",
-    databaseURL: "https://store-499f8.firebaseio.com",
-    projectId: "store-499f8",
-    storageBucket: "store-499f8.appspot.com",
-    messagingSenderId: "18126373511",
-    appId: "1:18126373511:web:7e991b896b78a9fb892e22",
-    measurementId: "G-1XDYF0ND2N"
+    apiKey: ",
+    authDomain: "",
+    databaseURL: "",            // <<<insert firebase config file here
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
   // Initialize Firebase
     
